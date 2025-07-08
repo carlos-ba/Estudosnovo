6ffaf6c42910aefcff5fbb240d745e7f556baf74
carlos-ba <64696065+carlos-ba@users.noreply.github.com>
2025-07-08 19:11:46 -0300

    Adicionar programa de concatenação de strings

---
+++ main.py
@@ -0,0 +1,9 @@
+# Lendo as duas variáveis do usuário
+primeira_variavel = input("Digite o primeiro valor: ")
+segunda_variavel = input("Digite o segundo valor: ")
+
+# Juntando (concatenando) as variáveis
+resultado = primeira_variavel + segunda_variavel
+
+# Imprimindo o resultado
+print("O resultado da junção é:", resultado)
